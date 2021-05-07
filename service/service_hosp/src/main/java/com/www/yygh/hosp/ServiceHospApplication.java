41543eab69f07c73fc4284cfafc953f7f8af4fb1
wangweiwei_P28 <wangweiwei.sh@chinaccs.cn>
2021-05-07 14:24:18 +0800

    提交：导入hosp等的实体类

---
+++ service/service_hosp/src/main/java/com/www/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,17 @@
+package com.www.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author www
+ * @version 1.0
+ * @create 2021/5/7 10:46
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+     public static void main(String[] args) {
+           SpringApplication.run(ServiceHospApplication.class, args);
+      }
+
+}
