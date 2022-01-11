85c979e9c4a217182297855f29e5ca33bbc64209
wangweiwei_P28 <wangweiwei.sh@chinaccs.cn>
2022-01-11 09:51:18 +0800

    提交：Nacos注册service-hosp和service-cmn

--- service/service_cmn/src/main/java/com/www/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/www/yygh/cmn/ServiceCmnApplication.java
@@ -2,6 +2,7 @@ package com.www.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.www"})
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class, args);
